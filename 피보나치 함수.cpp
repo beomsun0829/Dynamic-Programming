ab7e14ff5a035d8495027bde0d04055ee2882bc3
ISAAC FOSTER <ghsqjatns@naver.com>
2017-04-29 12:04:12 +0900

    1003 피보나치 함수
    https://www.acmicpc.net/problem/1003

---
+++ 피보나치 함수.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+using namespace std;
+
+int onec, zeroc;
+
+int fibonacci(int n) {
+	if (n == 0) {
+		zeroc++;
+		return 0;
+
+	}
+	else if (n == 1) {
+		onec++;
+		return 1;
+	}
+	else {
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
+
+int main(){
+	int t;
+	cin >> t;
+	while (t--){
+		int a;
+		cin >> a;
+		fibonacci(a);
+		printf("%d %d\n", zeroc, onec);
+		onec = zeroc = 0;
+	}
+	return 0;
+}
